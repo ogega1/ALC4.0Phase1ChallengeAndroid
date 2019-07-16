5830b45821e9f3cf0fe211d78c80731f35bf78f5
kelvin ogega <ogega.kelvin@gmail.com>
2019-07-16 21:24:34 +0300

    update

--- app/src/main/java/firstandroidapplication/javacodegeeks/com/alc4phase1challenge_android/About_ALC.java
+++ app/src/main/java/firstandroidapplication/javacodegeeks/com/alc4phase1challenge_android/About_ALC.java
@@ -2,13 +2,17 @@ package firstandroidapplication.javacodegeeks.com.alc4phase1challenge_android;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 public class About_ALC extends Activity {
+    WebView webView;
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -22,14 +26,33 @@ public class About_ALC extends Activity {
                     }
                 }
         );
-        WebView webView = (WebView) findViewById(R.id.webview);
+        webView = (WebView) findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("http://andela.com/alc/");
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new MyBrowser());
+        webView.loadUrl("https://andela.com/alc/");
 
 
 
     }
-    private void back2_btn(){
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl("https://digger.co.ke/nairobi-kenya/tenders/");
+            return true;
+        }
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            super.onReceivedSslError(view, handler, error);
+
+            // this will ignore the Ssl error and will go forward to your site
+            handler.proceed();
+        }
+    }
+
+
+
+    public void back2_btn(){
         Intent intent=new Intent(this,Main_Activity.class);
         startActivity(intent);
     }
